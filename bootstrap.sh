2000a59e752023171d3c4bb5e78613f49b977ef2
Diego Lorenzo <diego@diegolorenzo.com>
2015-08-22 15:29:40 -0400

    Updates to main bootstrap file.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,23 @@
 
 
 
+# Symlink Dotfiles
+# ------------------------------------------------------------------------------
+# Symlink necessary dotfiles into home directory.
+
+# Symlinking Code: Do the thing, win the points!
+# source ~/.bash_profile
+
+
+
+# Create Code Directory
+# ------------------------------------------------------------------------------
+# This directory will house all code projects and repos from GitHub.
+
+mkdir -p $HOME/Code
+
+
+
 # Install Homebrew
 # ------------------------------------------------------------------------------
 # Check if Homebrew exists and if not install it.
@@ -34,7 +51,7 @@ echo "Running brew cask shell script..."
 
 # Install non-Homebrew binary packages
 # ------------------------------------------------------------------------------
-# (might need to change persmissions on file to make executable)
+# (may need to change persmissions on files to make executable; confirm this...)
 
 echo "Running binary applications shell script..."
 ./mac_bins_installations.sh 

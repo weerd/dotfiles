0f1250d96308c74ca59e2a2f4e1495d64263468c
Diego Lorenzo <diego@diegolorenzo.com>
2015-11-28 17:14:16 -0500

    Updating OSX configurations.

--- osx.sh
+++ osx.sh
@@ -7,8 +7,15 @@
 
 
 
+source ./extras/colors
+
+
+
 # Ask for the administrator password upfront
 sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 
@@ -21,6 +28,14 @@ sudo nvram SystemAudioVolume=" "
 # Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
+
+
+# Finder
+# ------------------------------------------------------------------------------
+
+# Allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
@@ -28,11 +43,6 @@ defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 # Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-
-
-# Finder
-# ------------------------------------------------------------------------------
 
 # Save to disk (not to iCloud) by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
@@ -47,12 +57,31 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Hide status bar
-defaults write com.apple.finder ShowStatusBar -bool false
+# Show window status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Empty Trash securely by default
+defaults write com.apple.finder EmptyTrashSecurely -bool true
 
 # Set prefs for showing a few different volumes on the Desktop
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Save screenshots to the Screenshots directory
+defaults write com.apple.screencapture location -string "${HOME}/Screenshots"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Expand the following File Info panes:
+# “General”, “Open with”, and “Sharing & Permissions”
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+	General -bool true \
+	OpenWith -bool true \
+	Privileges -bool true
 
 
 
@@ -62,16 +91,45 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 # Set the icon size of Dock items to 46 pixels
 defaults write com.apple.dock tilesize -int 46
 
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+
+# Show indicator for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+
+
+# Dashboard & Spaces
+# ------------------------------------------------------------------------------
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
-# Speed up Mission Control animations
-# defaults write com.apple.dock expose-animation-duration -float 0.25
 
-# Hot corners
+
+# Mission Control
+# ------------------------------------------------------------------------------
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+
+
+
+# Hot Corners
+# ------------------------------------------------------------------------------
+
 # Possible values:
 #  0: no-op
 #  2: Mission Control
@@ -102,6 +160,19 @@ defaults write com.apple.dock wvous-br-modifier -int 0
 
 
 
+# Input Peripherals & Accessories
+# ------------------------------------------------------------------------------
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+
+
 # Safari
 # ------------------------------------------------------------------------------
 
@@ -113,3 +184,16 @@ defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.Web
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 
+
+# Kill affected applications
+for app in \
+	"Activity Monitor" \
+	"cfprefsd" \
+	"Dock" \
+	"Finder" \
+	"Safari" \
+	"Terminal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+
+printf "\n$INFO%s$RESET\n" "OSX configurations completed. Note that some of these changes require a logout/restart to take effect."
